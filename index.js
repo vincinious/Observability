f283215ac289a8429cbf403dc22b0547a36d62ea
vincinious <87542036+vincinious@users.noreply.github.com>
2024-12-07 18:26:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,33 +6,27 @@ app.use(express.json());
 const port = 3000;
 let db;
 
-const startServer = async () => 
-{
+const startServer = async () => {
     const client = await MongoClient.connect("mongodb://localhost:27017/");
     db = client.db("todo");
-    await db.collection("todos").insertMany
-    ([
+    await db.collection("todos").insertMany([
         { id: "1", title: "Buy groceries" },
         { id: "2", title: "Install Aspecto" },
         { id: "3", title: "buy my own name domain" },
     ]);
-  
-    app.listen(port, () => 
-    {
+    app.listen(port, () => {
         console.log(`Example app listening on port ${port}`);
     });
 };
 
 startServer();
 
-app.get("/todo", async (req, res) => 
-{
+app.get("/todo", async (req, res) => {
     const todos = await db.collection("todos").find({}).toArray();
     res.send(todos);
 });
 
-app.get("/todo/:id", async (req, res) =>
-{
+app.get("/todo/:id", async (req, res) => {
     const todo = await db.collection("todos").findOne({ id: req.params.id });
     res.send(todo);
 });
