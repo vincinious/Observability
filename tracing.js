3a5a7893a85b8b7cd8709f32f814eaa31cc639a7
vincinious <87542036+vincinious@users.noreply.github.com>
2024-12-07 20:28:00 -0500

    Update tracing.js
    integrated Jaeger exporter

--- tracing.js
+++ tracing.js
@@ -1,18 +1,19 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { ConsoleSpanExporter, SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
-const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
-const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 
-module.exports = (serviceName) => 
-    {
-
-    const exporter = new ConsoleSpanExporter();
-
+module.exports = (serviceName) => {
+    // Configured Jaeger Exporter
+    const jaegerExporter = new JaegerExporter({
+        endpoint: "http://localhost:14268/api/traces", // Jaeger's default endpoint for receiving traces
+    });
 
     const provider = new NodeTracerProvider({
         resource: new Resource({
@@ -20,14 +21,12 @@ module.exports = (serviceName) =>
         }),
     });
 
-
-    provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+    // Added the Jaeger Exporter to the provider
+    provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
     provider.register();
 
-
     registerInstrumentations({
-        instrumentations: 
-        [
+        instrumentations: [
             new HttpInstrumentation(),
             new ExpressInstrumentation(),
             new MongoDBInstrumentation(),
